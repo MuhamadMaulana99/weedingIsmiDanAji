0425d5941545ba81bbb553921d24ab689e180e40
arfather <arfather26@gmail.com>
2022-12-14 12:38:27 +0700

    typo map

--- src/components/Home/Layout/maps/Maps.jsx
+++ src/components/Home/Layout/maps/Maps.jsx
@@ -13,7 +13,7 @@ export const Maps = () => {
         </h1>
         <div>
           <iframe
-           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.7816970429235!2d106.63591571476951!3d-6.292396395445848!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa8936fb11fbf21a9!2zNsKwMTcnMzIuNiJTIDEwNsKwMzgnMTcuMiJF!5e0!3m2!1sid!2sid!4v1670917030081!5m2!1sid!2sid"
+           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1982.889362340906!2d106.63699185819418!3d-6.292785832812046!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69fb5af8c3daf7%3A0xc11e80118d25f586!2sJl.%20Kp.%20Tegal%2C%20Pagedangan%2C%20Kec.%20Pagedangan%2C%20Kabupaten%20Tangerang%2C%20Banten%2015339!5e0!3m2!1sid!2sid!4v1670995980601!5m2!1sid!2sid"
             width="180"
             height="180"
             // style="border:0;"
